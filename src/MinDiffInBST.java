d14b07f431d4a54011c03ddb4306b41e57edd56c
cye2277 <cye2277@gmail.com>
2018-10-22 12:38:06 -0500

    add MinDiffInBST

---
+++ src/MinDiffInBST.java
@@ -0,0 +1,58 @@
+import java.util.TreeSet;
+
+public class MinDiffInBST {
+
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            this.val = x;
+        }
+    }
+
+    //method 1
+    //in-order traverse
+    //Time complexity O(N)
+    //Space complexity O(1)
+    int min = Integer.MIN_VALUE;
+    Integer prev = null;
+    public int minDiffInBST(TreeNode root){
+        if (root == null){
+            return min;
+        }
+        minDiffInBST(root.left);
+        if (prev!= null){
+            min = Math.min(min,root.val-prev);
+        }
+        prev = root.val;
+        minDiffInBST(root.right);
+        return min;
+    }
+
+    //Follow-up
+    //if it is not a binary tree
+    //We can use the treeset, and then in preorder to go through each node
+    //Time complexity:O(NlognN) treeset use logN to find the nearest number in the treeset
+    //Space complexity: O(N)
+    TreeSet<Integer> set = new TreeSet<>();
+    int min2 = Integer.MIN_VALUE;
+    public int minDiffInBST1(TreeNode root){
+        if (root == null){
+            return min2;
+        }
+        if(!set.isEmpty()){
+            if (set.floor(root.val) != null){
+                min2 = Math.min(min2, root.val-set.floor(root.val));
+            }
+            if (set.ceiling(root.val)!= null){
+                min2 = Math.min(min2, set.ceiling(root.val) - root.val);
+            }
+
+        }
+        set.add(root.val);
+        minDiffInBST1(root.left);
+        minDiffInBST1(root.right);
+        return  min2;
+    }
+}
