4ac319f429fd4e608ef9e5331cb72c912c7af104
cye2277 <cye2277@gmail.com>
2019-10-19 11:28:41 -0700

    Add new question

--- src/AllPossibleFBT.java
+++ src/AllPossibleFBT.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -27,11 +28,28 @@ public class AllPossibleFBT {
         }
     }
 
-    Map<Integer, List<TreeNode>> meomo = new HashMap<>();
 
-//    public List<TreeNode> allPossibleFBT(int N) {
-//        int[] nums = new int[]{1,2,3};
-//
-//        return;
-//    }
+    Map<Integer, List<TreeNode>> memo = new HashMap();
+    public List<TreeNode> allPossibleFBT(int N) {
+        if (!memo.containsKey(N)){
+            List<TreeNode> ans = new LinkedList();
+            if (N== 1){
+                ans.add(new TreeNode(0));
+            }else if(N %2 == 1){
+                for (int x = 0; x< N;x++){
+                    int y = N-1-x;
+                    for (TreeNode left: allPossibleFBT(x)){
+                        for (TreeNode right: allPossibleFBT(y)){
+                            TreeNode bns = new TreeNode(0);
+                            bns.left = left;
+                            bns.right = right;
+                            ans.add(bns);
+                        }
+                    }
+                }
+            }
+            memo.put(N, ans);
+        }
+        return memo.get(N);
+    }
 }
